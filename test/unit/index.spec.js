120f673d99b46a9ba8f1ead729f53a107e5dd147
Stefan Walther <Stefan.Walther@qlik.com>
2016-11-29 13:56:20 -0500

    chore: Add winston-console-formatter

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -1,10 +1,16 @@
-/*global describe, it*/
+/*global describe, it, beforeEach*/
 import winster from './../../src/index';
 
 describe( 'test', () => {
 
+  let logger;
+  beforeEach( () => {
+    logger = new winster();
+  } );
+
   it( 'should ideally succeed', () => {
     expect( true ).to.be.true;
+    logger.silly( 'hurray' );
   } );
 
 } );
